07ac629452fd81c8a38695f08c1a42451ddbe328
Akosh Farkash <akosh.farkash@iohk.io>
2021-03-04 14:09:40 +0000

    PM-2903: Database creation.

--- metronome/rocksdb/src/io/iohk/metronome/rocksdb/RocksDBStore.scala
+++ metronome/rocksdb/src/io/iohk/metronome/rocksdb/RocksDBStore.scala
@@ -11,20 +11,30 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 import org.rocksdb.{
   RocksDB,
   WriteBatch,
-  ColumnFamilyHandle,
   WriteOptions,
-  ReadOptions
+  ReadOptions,
+  Options,
+  DBOptions,
+  ColumnFamilyOptions,
+  ColumnFamilyDescriptor,
+  ColumnFamilyHandle,
+  BlockBasedTableConfig,
+  BloomFilter,
+  CompressionType,
+  ClockCache
 }
 import scodec.Codec
 import scodec.bits.BitVector
+import scala.collection.mutable
+import java.nio.file.Path
 
 class RocksDBStore[F[_]: Sync](
     db: RocksDB,
-    handles: Map[RocksDBStore.Namespace, ColumnFamilyHandle],
     readOptions: ReadOptions,
-    rwlock: ReentrantReadWriteLock
+    rwlock: ReentrantReadWriteLock,
+    handles: Map[RocksDBStore.Namespace, ColumnFamilyHandle]
 ) {
-  import RocksDBStore.{Namespace, DBQuery}
+  import RocksDBStore.{Namespace, DBQuery, autoCloseableR}
 
   type BatchEnv          = (WriteOptions, WriteBatch)
   type Batch[A]          = ({ type L[A] = ReaderT[F, BatchEnv, A] })#L[A]
@@ -134,15 +144,12 @@ class RocksDBStore[F[_]: Sync](
   private def decode[T](bytes: Array[Byte])(implicit ev: Codec[T]): F[T] =
     Sync[F].fromTry(ev.decodeValue(BitVector(bytes)).toTry)
 
-  private def fromAutoCloseable[R <: AutoCloseable](mk: => R): Resource[F, R] =
-    Resource.fromAutoCloseable[F, R](Sync[F].delay(mk))
-
   private def runWithBatchingNoLock[A](
       program: KVStore[Namespace, A]
   ): DBQuery[F, A] = {
     val env = for {
-      opts  <- fromAutoCloseable(new WriteOptions())
-      batch <- fromAutoCloseable(new WriteBatch())
+      opts  <- autoCloseableR(new WriteOptions())
+      batch <- autoCloseableR(new WriteBatch())
     } yield (opts, batch)
 
     env.use {
@@ -192,5 +199,146 @@ object RocksDBStore {
     */
   type DBQuery[F[_], A] = F[A]
 
-  def apply[F[_]: Sync](): Resource[F, RocksDBStore[F]] = ???
+  case class Config(
+      path: Path,
+      createIfMissing: Boolean,
+      paranoidChecks: Boolean,
+      maxThreads: Int,
+      maxOpenFiles: Int,
+      verifyChecksums: Boolean,
+      levelCompaction: Boolean,
+      blockSizeBytes: Long,
+      blockCacheSizeBytes: Long
+  )
+  object Config {
+    def default(path: Path): Config =
+      Config(
+        path = path,
+        // Create DB data directory if it's missing
+        createIfMissing = true,
+        // Should the DB raise an error as soon as it detects an internal corruption
+        paranoidChecks = true,
+        maxThreads = 1,
+        maxOpenFiles = 32,
+        // Force checksum verification of all data that is read from the file system on behalf of a particular read.
+        verifyChecksums = true,
+        // In this mode, size target of levels are changed dynamically based on size of the last level.
+        // https://rocksdb.org/blog/2015/07/23/dynamic-level.html
+        levelCompaction = true,
+        // Approximate size of user data packed per block (16 * 1024)
+        blockSizeBytes = 16384,
+        // Amount of cache in bytes that will be used by RocksDB (32 * 1024 * 1024)
+        blockCacheSizeBytes = 33554432
+      )
+  }
+
+  def apply[F[_]: Sync](
+      config: Config,
+      namespaces: Seq[Namespace]
+  ): Resource[F, RocksDBStore[F]] = {
+    import scala.collection.JavaConverters._
+
+    // There is a specific order for closing RocksDB with column families described in
+    // https://github.com/facebook/rocksdb/wiki/RocksJava-Basics#opening-a-database-with-column-families
+    // 1. Free all column families handles
+    // 2. Free DB and DB options
+    // 3. Free column families options
+    // So they are created in the opposite order.
+    for {
+      _ <- Resource.liftF[F, Unit](Sync[F].delay {
+        RocksDB.loadLibrary()
+      })
+
+      tableConf <- Resource.pure[F, BlockBasedTableConfig] {
+        mkTableConfig(config)
+      }
+
+      cfOpts <- autoCloseableR[F, ColumnFamilyOptions] {
+        new ColumnFamilyOptions()
+          .setCompressionType(CompressionType.LZ4_COMPRESSION)
+          .setBottommostCompressionType(CompressionType.ZSTD_COMPRESSION)
+          .setLevelCompactionDynamicLevelBytes(config.levelCompaction)
+          .setTableFormatConfig(tableConf)
+      }
+
+      cfDescriptors =
+        new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY, cfOpts) +:
+          namespaces.map(n => new ColumnFamilyDescriptor(n.toArray, cfOpts))
+
+      dbOpts <- autoCloseableR[F, DBOptions] {
+        new DBOptions()
+          .setCreateIfMissing(config.createIfMissing)
+          .setParanoidChecks(config.paranoidChecks)
+          .setMaxOpenFiles(config.maxOpenFiles)
+          .setIncreaseParallelism(config.maxThreads)
+          .setCreateMissingColumnFamilies(true)
+      }
+
+      readOpts <- autoCloseableR[F, ReadOptions] {
+        new ReadOptions().setVerifyChecksums(config.verifyChecksums)
+      }
+
+      // The handles will be filled as the database is opened.
+      columnFamilyHandleBuffer = mutable.Buffer.empty[ColumnFamilyHandle]
+
+      db <- autoCloseableR[F, RocksDB] {
+        RocksDB.open(
+          dbOpts,
+          config.path.toString,
+          cfDescriptors.asJava,
+          columnFamilyHandleBuffer.asJava
+        )
+      }
+
+      columnFamilyHandles <- Resource.make(
+        Sync[F].delay {
+          assert(columnFamilyHandleBuffer.size == namespaces.size)
+          (namespaces zip columnFamilyHandleBuffer).toMap
+        }
+      ) { handles =>
+        Sync[F].delay(handles.values.foreach(_.close()))
+      }
+
+      store = new RocksDBStore[F](
+        db,
+        readOpts,
+        new ReentrantReadWriteLock(),
+        columnFamilyHandles
+      )
+
+    } yield store
+  }
+
+  def destroy[F[_]: Sync](
+      config: Config
+  ): F[Unit] = {
+    autoCloseableR[F, Options] {
+      new Options()
+        .setCreateIfMissing(config.createIfMissing)
+        .setParanoidChecks(config.paranoidChecks)
+        .setCompressionType(CompressionType.LZ4_COMPRESSION)
+        .setBottommostCompressionType(CompressionType.ZSTD_COMPRESSION)
+        .setLevelCompactionDynamicLevelBytes(config.levelCompaction)
+        .setMaxOpenFiles(config.maxOpenFiles)
+        .setIncreaseParallelism(config.maxThreads)
+        .setTableFormatConfig(mkTableConfig(config))
+    }.use { options =>
+      Sync[F].delay {
+        RocksDB.destroyDB(config.path.toString, options)
+      }
+    }
+  }
+
+  private def mkTableConfig(config: Config): BlockBasedTableConfig =
+    new BlockBasedTableConfig()
+      .setBlockSize(config.blockSizeBytes)
+      .setBlockCache(new ClockCache(config.blockCacheSizeBytes))
+      .setCacheIndexAndFilterBlocks(true)
+      .setPinL0FilterAndIndexBlocksInCache(true)
+      .setFilterPolicy(new BloomFilter(10, false))
+
+  private def autoCloseableR[F[_]: Sync, R <: AutoCloseable](
+      mk: => R
+  ): Resource[F, R] =
+    Resource.fromAutoCloseable[F, R](Sync[F].delay(mk))
 }
