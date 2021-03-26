63399b184a7ac0e8a4f0c9943913c481ec0b95b9
Akosh Farkash <akosh.farkash@iohk.io>
2021-03-26 15:44:03 +0000

    PM-3093: Add publish script.

---
+++ .circleci/publish.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euv
+
+echo $GPG_KEY | base64 --decode | gpg --batch --import
+
+gpg --passphrase $GPG_PASSPHRASE --batch --yes -a -b LICENSE
+
+if [[ "$CIRCLE_BRANCH" == "develop" || "$CIRCLE_BRANCH" == "PM-3093-publish" ]]; then
+
+mill mill.scalalib.PublishModule/publishAll \
+    __.publishArtifacts \
+    "$OSS_USERNAME":"$OSS_PASSWORD" \
+    --gpgArgs --passphrase="$GPG_PASSPHRASE",--batch,--yes,-a,-b
+
+elif [[ "$CIRCLE_BRANCH" == "master" ]]; then
+
+mill versionFile.setReleaseVersion
+mill mill.scalalib.PublishModule/publishAll \
+    __.publishArtifacts \
+    "$OSS_USERNAME":"$OSS_PASSWORD" \
+    --gpgArgs --passphrase="$GPG_PASSPHRASE",--batch,--yes,-a,-b \
+    --readTimeout 600000 \
+    --awaitTimeout 600000 \
+    --release true
+
+else
+
+  echo "Skipping publish step"
+
+fi
