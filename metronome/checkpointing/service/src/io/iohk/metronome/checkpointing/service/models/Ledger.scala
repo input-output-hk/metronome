cf0945bf2270f3a39bd6817515ebff88123fed0c
Akosh Farkash <akosh.farkash@iohk.io>
2021-03-30 17:47:35 +0100

    PM-2931: Basic ledger.

---
+++ metronome/checkpointing/service/src/io/iohk/metronome/checkpointing/service/models/Ledger.scala
@@ -0,0 +1,37 @@
+package io.iohk.metronome.checkpointing.service.models
+
+import io.iohk.metronome.core.Validated
+import io.iohk.metronome.checkpointing.interpreter.models.Transaction
+
+/** Current state of the ledger after applying all previous blocks.
+  *
+  * Basically it's the last checkpoint, plus any accumulated proposer blocks
+  * since then. Initially the last checkpoint is empty; conceptually it could
+  * the the genesis block of the PoW chain, but we don't know what that is
+  * until we talk to the interpreter, and we also can't produce it on our
+  * own since it's opaque data.
+  */
+case class Ledger(
+    maybeLastCheckpoint: Option[Transaction.CheckpointCandidate],
+    proposerBlocks: Set[Transaction.ProposerBlock]
+) {
+
+  /** Apply a validated transaction to produce the next ledger state.
+    *
+    * The transaction should have been validated against the PoW ledger
+    * by this point, so we know for example that the new checkpoint is
+    * a valid extension of the previous one.
+    */
+  def apply(transaction: Validated[Transaction]): Ledger =
+    (transaction: Transaction) match {
+      case t @ Transaction.ProposerBlock(_) =>
+        copy(proposerBlocks = proposerBlocks + t)
+
+      case t @ Transaction.CheckpointCandidate(_) =>
+        Ledger(Some(t), Set.empty)
+    }
+}
+
+object Ledger {
+  val empty = Ledger(None, Set.empty)
+}
