1cc0150739c6834ba816eb1c409a8540c6bbe706
Akosh Farkash <akosh.farkash@iohk.io>
2021-03-31 11:33:16 +0100

    PM-2931: Simplify property declaration.

--- metronome/checkpointing/service/test/src/io/iohk/metronome/checkpointing/service/models/RLPCodecsProps.scala
+++ metronome/checkpointing/service/test/src/io/iohk/metronome/checkpointing/service/models/RLPCodecsProps.scala
@@ -4,16 +4,20 @@ import io.iohk.ethereum.rlp
 import io.iohk.ethereum.rlp.RLPCodec
 import org.scalacheck._
 import org.scalacheck.Prop.forAll
+import scala.reflect.ClassTag
 
 object RLPCodecsProps extends Properties("RLPCodecs") {
   import ArbitraryInstances._
   import RLPCodecs._
 
-  def roundtrip[T: RLPCodec: Arbitrary]: Prop = forAll { (value0: T) =>
-    val bytes  = rlp.encode(value0)
-    val value1 = rlp.decode[T](bytes)
-    value0 == value1
-  }
+  /** Test that encoding to and decoding from RLP preserves the value. */
+  def propRoundTrip[T: RLPCodec: Arbitrary: ClassTag] =
+    property(implicitly[ClassTag[T]].runtimeClass.getSimpleName) = forAll {
+      (value0: T) =>
+        val bytes  = rlp.encode(value0)
+        val value1 = rlp.decode[T](bytes)
+        value0 == value1
+    }
 
-  property("ledger") = roundtrip[Ledger]
+  propRoundTrip[Ledger]
 }
