9b8d8e41cb782221dfcc3164e4eaa9a2690b4e22
Akosh Farkash <akosh.farkash@iohk.io>
2021-04-13 12:18:43 +0100

    PM-3146: Use the FiberPool in SyncService.

--- metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/SyncService.scala
+++ metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/SyncService.scala
@@ -5,10 +5,12 @@ import cats.effect.{Sync, Resource, Concurrent}
 import io.iohk.metronome.hotstuff.consensus.basic.Agreement
 import io.iohk.metronome.hotstuff.service.messages.SyncMessage
 import io.iohk.metronome.networking.ConnectionHandler
+import cats.effect.ContextShift
 
 class SyncService[F[_]: Sync, A <: Agreement](
     network: Network[F, A, SyncMessage[A]],
-    consensusService: ConsensusService[F, A]
+    consensusService: ConsensusService[F, A],
+    fiberPool: FiberPool[F, A#PKey]
 ) {
 
   /** Process incoming network messages */
@@ -17,32 +19,42 @@ class SyncService[F[_]: Sync, A <: Agreement](
     // TODO (PM-3186): Rate limiting per source.
     network.incomingMessages
       .mapEval[Unit] { case ConnectionHandler.MessageReceived(from, message) =>
-        message match {
-          case GetStatusRequest(requestId) =>
-            consensusService.getState.flatMap { state =>
-              val status =
-                Status(state.viewNumber, state.prepareQC, state.commitQC)
-              network.sendMessage(from, GetStatusResponse(requestId, status))
-            }
+        val handler: F[Unit] =
+          message match {
+            case GetStatusRequest(requestId) =>
+              consensusService.getState.flatMap { state =>
+                val status =
+                  Status(state.viewNumber, state.prepareQC, state.commitQC)
+                network.sendMessage(
+                  from,
+                  GetStatusResponse(requestId, status)
+                )
+              }
 
-          case GetBlockRequest(requestId, blockHash) =>
-            // TODO (PM-3134): Retreive block from storage and respond.
-            val getBlock: F[Option[A#Block]] = ???
+            case GetBlockRequest(requestId, blockHash) =>
+              // TODO (PM-3134): Retreive block from storage and respond.
+              val getBlock: F[Option[A#Block]] = ???
 
-            getBlock.flatMap {
-              case None => ().pure[F]
-              case Some(block) =>
-                network.sendMessage(from, GetBlockResponse(requestId, block))
-            }
+              getBlock.flatMap {
+                case None => ().pure[F]
+                case Some(block) =>
+                  network.sendMessage(
+                    from,
+                    GetBlockResponse(requestId, block)
+                  )
+              }
 
-          case GetStatusResponse(requestId, status) =>
-            // TODO (PM-3063): Hand over to view synchronisation.
-            ???
+            case GetStatusResponse(requestId, status) =>
+              // TODO (PM-3063): Hand over to view synchronisation.
+              ???
 
-          case GetBlockResponse(requestId, block) =>
-            // TODO (PM-3134): Hand over to block synchronisation.
-            ???
-        }
+            case GetBlockResponse(requestId, block) =>
+              // TODO (PM-3134): Hand over to block synchronisation.
+              ???
+          }
+
+        // Handle on a fiber dedicated to the source.
+        fiberPool.submit(from)(handler).void
       }
       .completedL
   }
@@ -54,17 +66,14 @@ object SyncService {
     * in the background, shutting processing down when the resource is
     * released.
     */
-  def apply[F[_]: Concurrent, A <: Agreement](
+  def apply[F[_]: Concurrent: ContextShift, A <: Agreement](
       network: Network[F, A, SyncMessage[A]],
       consensusService: ConsensusService[F, A]
   ): Resource[F, SyncService[F, A]] =
     // TODO (PM-3187): Add Tracing
     for {
-      service <- Resource.liftF {
-        Sync[F].delay {
-          new SyncService[F, A](network, consensusService)
-        }
-      }
+      fiberPool <- FiberPool[F, A#PKey]
+      service = new SyncService[F, A](network, consensusService, fiberPool)
       _ <- Concurrent[F].background(service.processMessages)
     } yield service
 }
