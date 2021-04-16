3a9690550afe23612af1a01c6623fc39d044e356
Akosh Farkash <akosh.farkash@iohk.io>
2021-04-16 16:46:16 +0100

    PM-3187: SyncService traces.

--- metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/tracing/SyncTracers.scala
+++ metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/tracing/SyncTracers.scala
@@ -1,5 +1,22 @@
 package io.iohk.metronome.hotstuff.service.tracing
 
+import cats.implicits._
+import io.iohk.metronome.tracer.Tracer
 import io.iohk.metronome.hotstuff.consensus.basic.Agreement
 
-case class SyncTracers[F[_], A <: Agreement]()
+case class SyncTracers[F[_], A <: Agreement](
+    queueFull: Tracer[F, A#PKey],
+    error: Tracer[F, Throwable]
+)
+
+object SyncTracers {
+  import SyncEvent._
+
+  def apply[F[_], A <: Agreement](
+      tracer: Tracer[F, SyncEvent[A]]
+  ): SyncTracers[F, A] =
+    SyncTracers[F, A](
+      queueFull = tracer.contramap[A#PKey](QueueFull(_)),
+      error = tracer.contramap[Throwable](Error(_))
+    )
+}
