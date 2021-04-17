27616af9bb6ff731e53f206c13635bf05037dff7
Akosh Farkash <akosh.farkash@iohk.io>
2021-04-17 08:58:31 +0100

    PM-3104: Add KVStore.alter method.

--- metronome/storage/src/io/iohk/metronome/storage/KVStore.scala
+++ metronome/storage/src/io/iohk/metronome/storage/KVStore.scala
@@ -31,6 +31,7 @@ object KVStore {
 
     def pure[A](a: A) = KVStore.pure[N, A](a)
 
+    /** Insert or replace a value under a key. */
     def put[K: Codec, V: Codec](
         namespace: N,
         key: K,
@@ -40,16 +41,19 @@ object KVStore {
         Put[N, K, V](namespace, key, value)
       )
 
+    /** Get a value under a key, if it exists. */
     def get[K: Codec, V: Codec](namespace: N, key: K): KVStore[N, Option[V]] =
       liftF[KVNamespacedOp, Option[V]](
         Get[N, K, V](namespace, key)
       )
 
+    /** Delete a value under a key. */
     def delete[K: Codec](namespace: N, key: K): KVStore[N, Unit] =
       liftF[KVNamespacedOp, Unit](
         Delete[N, K](namespace, key)
       )
 
+    /** Apply a function on a value, if it exists. */
     def update[K: Codec, V: Codec](
         namespace: N,
         key: K,
@@ -58,6 +62,20 @@ object KVStore {
       get[K, V](namespace, key).flatMap {
         case None        => unit
         case Some(value) => put(namespace, key, f(value))
+      }
+
+    /** Insert, update or delete a value, depending on whether it exists. */
+    def alter[K: Codec, V: Codec](
+        namespace: N,
+        key: K,
+        f: Option[V] => Option[V]
+    ): KVStore[N, Unit] =
+      get[K, V](namespace, key).flatMap { current =>
+        (current, f(current)) match {
+          case (None, None)     => unit
+          case (_, Some(value)) => put(namespace, key, value)
+          case (Some(_), None)  => delete(namespace, key)
+        }
       }
 
     /** Lift a read-only operation into a read-write one, so that we can chain them together. */
