52d78efa1bac489d9f4203fa2e5cfdd3e1d9dac3
Akosh Farkash <akosh.farkash@iohk.io>
2021-04-19 13:11:25 +0100

    PM-3112: Set view state components, get bundle.

--- metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/storage/ViewStateStorage.scala
+++ metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/storage/ViewStateStorage.scala
@@ -1,29 +1,78 @@
 package io.iohk.metronome.hotstuff.service.storage
 
+import cats.implicits._
 import io.iohk.metronome.hotstuff.consensus.ViewNumber
-import io.iohk.metronome.hotstuff.consensus.basic.{Agreement, QuorumCertificate}
-import io.iohk.metronome.storage.KVStore
-import scodec.Codec
+import io.iohk.metronome.hotstuff.consensus.basic.{
+  Agreement,
+  QuorumCertificate,
+  Phase
+}
+import io.iohk.metronome.storage.{KVStore, KVStoreRead}
+import scodec.{Codec, Encoder, Decoder}
 
-/** Storing elements of the view state individually under separate keys,
-  * because they get written independently.
-  */
-class ViewStateStorage[N, A <: Agreement](namespace: N)(implicit
+class ViewStateStorage[N, A <: Agreement] private (
+    namespace: N
+)(implicit
+    keys: ViewStateStorage.Keys[A],
     kvn: KVStore.Ops[N],
+    kvrn: KVStoreRead.Ops[N],
     codecVN: Codec[ViewNumber],
     codecQC: Codec[QuorumCertificate[A]],
     codecH: Codec[A#Hash]
-) {}
+) {
+  import keys.Key
+
+  private def put[V: Encoder](key: Key[V], value: V) =
+    KVStore[N].put[Key[V], V](namespace, key, value)
+
+  private def read[V: Decoder](key: Key[V]): KVStoreRead[N, V] =
+    KVStoreRead[N].read[Key[V], V](namespace, key).map(_.get)
+
+  def setViewNumber(viewNumber: ViewNumber): KVStore[N, Unit] =
+    put(Key.ViewNumber, viewNumber)
+
+  def setQuorumCertificate(qc: QuorumCertificate[A]): KVStore[N, Unit] =
+    qc.phase match {
+      case Phase.Prepare =>
+        put(Key.PrepareQC, qc)
+      case Phase.PreCommit =>
+        put(Key.LockedQC, qc)
+      case Phase.Commit =>
+        put(Key.CommitQC, qc)
+    }
+
+  def setLastExecutedBlockHash(blockHash: A#Hash): KVStore[N, Unit] =
+    put(Key.LastExecutedBlockHash, blockHash)
+
+  def getBundle: KVStoreRead[N, ViewStateStorage.Bundle[A]] =
+    (
+      read(Key.ViewNumber),
+      read(Key.PrepareQC),
+      read(Key.LockedQC),
+      read(Key.CommitQC),
+      read(Key.LastExecutedBlockHash)
+    ).mapN(ViewStateStorage.Bundle.apply[A] _)
+
+}
 
 object ViewStateStorage {
 
-  sealed trait Key
-  object Key {
-    case object ViewNumber            extends Key
-    case object PrepareQC             extends Key
-    case object LockedQC              extends Key
-    case object CommitQC              extends Key
-    case object LastExecutedBlockHash extends Key
+  /** Storing elements of the view state individually under separate keys,
+    * because they get written independently.
+    */
+  trait Keys[A <: Agreement] {
+    sealed trait Key[V]
+    object Key {
+      case object ViewNumber            extends Key[ViewNumber]
+      case object PrepareQC             extends Key[QuorumCertificate[A]]
+      case object LockedQC              extends Key[QuorumCertificate[A]]
+      case object CommitQC              extends Key[QuorumCertificate[A]]
+      case object LastExecutedBlockHash extends Key[A#Hash]
+
+      implicit def encoder[V]: Encoder[Key[V]] =
+        scodec.codecs.implicits.implicitStringCodec
+          .contramap[Key[V]](_.toString)
+    }
   }
 
   /** The state of consensus that needs to be persisted between restarts.
@@ -51,7 +100,10 @@ object ViewStateStorage {
       codecQC: Codec[QuorumCertificate[A]],
       codecH: Codec[A#Hash]
   ): KVStore[N, ViewStateStorage[N, A]] = {
-    implicit val kvn = KVStore.instance[N]
+    implicit val kvn  = KVStore.instance[N]
+    implicit val kvrn = KVStoreRead.instance[N]
+    implicit val keys = new Keys[A] {}
+    import keys.Key
 
     def setDefault[V](default: V): Option[V] => Option[V] =
       (current: Option[V]) => current orElse Some(default)
