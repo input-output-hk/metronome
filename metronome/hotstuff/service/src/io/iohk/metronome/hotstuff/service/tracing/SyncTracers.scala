23c34ca28efb5f796b6dd7aa2badfc9dff227ec7
Akosh Farkash <akosh.farkash@iohk.io>
2021-04-20 16:07:04 +0100

    PM-3134: Use RPCTracker in SyncService.

--- metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/tracing/SyncTracers.scala
+++ metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/tracing/SyncTracers.scala
@@ -3,9 +3,11 @@ package io.iohk.metronome.hotstuff.service.tracing
 import cats.implicits._
 import io.iohk.metronome.tracer.Tracer
 import io.iohk.metronome.hotstuff.consensus.basic.Agreement
+import io.iohk.metronome.hotstuff.service.messages.SyncMessage
 
 case class SyncTracers[F[_], A <: Agreement](
     queueFull: Tracer[F, A#PKey],
+    responseIgnored: Tracer[F, SyncMessage[A] with SyncMessage.Response],
     error: Tracer[F, Throwable]
 )
 
@@ -17,6 +19,10 @@ object SyncTracers {
   ): SyncTracers[F, A] =
     SyncTracers[F, A](
       queueFull = tracer.contramap[A#PKey](QueueFull(_)),
+      responseIgnored = tracer
+        .contramap[SyncMessage[A] with SyncMessage.Response](
+          ResponseIgnored(_)
+        ),
       error = tracer.contramap[Throwable](Error(_))
     )
 }
