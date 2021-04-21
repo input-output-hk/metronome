89e1cdc4c14d141f0b40590c4316527d963f4fc4
Akosh Farkash <akosh.farkash@iohk.io>
2021-04-21 12:20:49 +0100

    PM-3134: Add cancel by key support to FiberMap.

--- metronome/core/src/io/iohk/metronome/core/fibers/DeferredTask.scala
+++ metronome/core/src/io/iohk/metronome/core/fibers/DeferredTask.scala
@@ -4,12 +4,14 @@ import cats.implicits._
 import cats.effect.Sync
 import cats.effect.concurrent.Deferred
 import cats.effect.Concurrent
+import scala.util.control.NoStackTrace
 
 /** A task that can be executed on a fiber pool, or canceled if the pool is shut down.. */
 protected[fibers] class DeferredTask[F[_]: Sync, A](
     deferred: Deferred[F, Either[Throwable, A]],
     task: F[A]
 ) {
+  import DeferredTask.CanceledException
 
   /** Execute the task and set the success/failure result on the deferred. */
   def execute: F[Unit] =
@@ -19,15 +21,19 @@ protected[fibers] class DeferredTask[F[_]: Sync, A](
   def join: F[A] =
     deferred.get.rethrow
 
-  /** Signal to the submitter that the pool has been shut down. */
-  def shutdown: F[Unit] =
+  /** Signal to the submitter that this task is canceled. */
+  def cancel: F[Unit] =
     deferred
-      .complete(Left(new RuntimeException("The pool has been shut down.")))
+      .complete(Left(new CanceledException))
       .attempt
       .void
 }
 
 object DeferredTask {
+  class CanceledException
+      extends RuntimeException("This task has been canceled.")
+      with NoStackTrace
+
   def apply[F[_]: Concurrent, A](task: F[A]): F[DeferredTask[F, A]] =
     Deferred[F, Either[Throwable, A]].map { d =>
       new DeferredTask[F, A](d, task)
