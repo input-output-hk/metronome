a82fdd98fef359a616222db7af5f8fac773750c3
Akosh Farkash <akosh.farkash@iohk.io>
2021-04-30 22:18:10 +0100

    PM-2941: Allow environment overrides.

--- metronome/config/src/io/iohk/metronome/config/ConfigParser.scala
+++ metronome/config/src/io/iohk/metronome/config/ConfigParser.scala
@@ -1,10 +1,12 @@
 package io.iohk.metronome.config
 
+import cats.implicits._
+import com.typesafe.config.{ConfigObject, ConfigRenderOptions}
 import io.circe.{Json, JsonObject, ParsingFailure}
 import io.circe.parser.parse
-import com.typesafe.config.{ConfigObject, ConfigRenderOptions}
 
 object ConfigParser {
+  type ParsingResult = Either[ParsingFailure, Json]
 
   /** Render a TypeSafe Config section into JSON. */
   def toJson(conf: ConfigObject): Json = {
@@ -19,21 +21,6 @@ object ConfigParser {
       case Right(json) =>
         json
     }
-  }
-
-  /** Transform all keys into camelCase form,
-    * so they can be matched to case class fields.
-    */
-  def toCamelCase(json: Json): Json = {
-    json
-      .mapArray { arr =>
-        arr.map(toCamelCase)
-      }
-      .mapObject { obj =>
-        JsonObject(obj.toIterable.map { case (key, value) =>
-          toCamelCase(key) -> toCamelCase(value)
-        }.toList: _*)
-      }
   }
 
   /** Transform a key in the HOCON config file to camelCase. */
@@ -51,13 +38,106 @@ object ConfigParser {
     loop(key.toList, Nil)
   }
 
-  /** Apply overrides from the environment to a JSON structure. */
-  // def withEnvVarOverrides(json: Json, prefix: String): Json = {
-  //   def loop(json: Json, path: ): Json =
-  // }
-
-  /** Turn `camelCaseKey` into `CAMEL_CASE_KEY`,
+  /** Turn `camelCaseKey` into `SNAKE_CASE_KEY`,
     * which is what it would look like as an env var.
     */
-  // def snakeify(camelCase: String): String = ???
+  def toSnakeCase(camelCase: String): String = {
+    def loop(cs: List[Char], acc: List[Char]): String =
+      cs match {
+        case a :: b :: cs if a.isLower && b.isUpper =>
+          loop(cs, b :: '_' :: a.toUpper :: acc)
+        case '-' :: cs =>
+          loop(cs, '_' :: acc)
+        case a :: cs =>
+          loop(cs, a.toUpper :: acc)
+        case Nil =>
+          acc.reverse.mkString
+      }
+    loop(camelCase.toList, Nil)
+  }
+
+  /** Transform all keys into camelCase form,
+    * so they can be matched to case class fields.
+    */
+  def withCamelCase(json: Json): Json = {
+    json
+      .mapArray { arr =>
+        arr.map(withCamelCase)
+      }
+      .mapObject { obj =>
+        JsonObject(obj.toIterable.map { case (key, value) =>
+          toCamelCase(key) -> withCamelCase(value)
+        }.toList: _*)
+      }
+  }
+
+  /** Apply overrides from the environment to a JSON structure.
+    *
+    * Only considers env var keys that start with prefix and are
+    * in a PREFIX_SNAKE_CASE format.
+    */
+  def withEnvVarOverrides(
+      json: Json,
+      prefix: String,
+      env: Map[String, String] = sys.env
+  ): ParsingResult = {
+    def extend(path: String, key: String) =
+      if (path.isEmpty) key else s"${path}_${key}"
+
+    def loop(json: Json, path: String): ParsingResult = {
+
+      def tryParse(
+          default: => Json,
+          validate: Json => Boolean
+      ): ParsingResult =
+        env
+          .get(path)
+          .map { value =>
+            val maybeJson = parse(value) orElse parse(s""""$value"""")
+
+            maybeJson.flatMap { json =>
+              if (validate(json)) {
+                Right(json)
+              } else {
+                val msg = s"Invalid value for $path: $value"
+                Left(ParsingFailure(value, new IllegalArgumentException(msg)))
+              }
+            }
+          }
+          .getOrElse(Right(default))
+
+      json
+        .fold[ParsingResult](
+          jsonNull = tryParse(Json.Null, _ => true),
+          jsonBoolean = x => tryParse(Json.fromBoolean(x), _.isBoolean),
+          jsonNumber = x => tryParse(Json.fromJsonNumber(x), _.isNumber),
+          jsonString = x => tryParse(Json.fromString(x), _.isString),
+          jsonArray = { arr =>
+            arr.zipWithIndex
+              .map { case (value, idx) =>
+                loop(value, extend(path, idx.toString))
+              }
+              .sequence
+              .map { values =>
+                Json.arr(values: _*)
+              }
+          },
+          jsonObject = { obj =>
+            obj.toIterable
+              .map { case (key, value) =>
+                val snakeKey = toSnakeCase(key)
+                loop(value, extend(path, snakeKey)).map(key ->)
+              }
+              .toList
+              .sequence
+              .map { values =>
+                Json.obj(values: _*)
+              }
+          }
+        )
+    }
+
+    loop(json, prefix)
+  }
+
 }
--- metronome/config/test/src/io/iohk/metronome/config/ConfigParserSpec.scala
+++ metronome/config/test/src/io/iohk/metronome/config/ConfigParserSpec.scala
@@ -4,35 +4,89 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.Inside
 
 class ConfigParserSpec
     extends AnyFlatSpec
     with Matchers
-    with TableDrivenPropertyChecks {
+    with TableDrivenPropertyChecks
+    with Inside {
   "toJson" should "parse simple.conf to JSON" in {
     val conf = ConfigFactory.load("simple.conf")
     val json = ConfigParser.toJson(conf.getConfig("simple").root())
     json.noSpaces shouldBe """{"nested-structure":{"bar_baz":{"spam":"eggs"},"foo":10}}"""
   }
 
-  "toCamelCase" should "turn keys into camel case" in {
+  "toCamelCase" should "turn keys into camelCase" in {
     val examples = Table(
       ("input", "expected"),
       ("nested-structure", "nestedStructure"),
       ("nested_structure", "nestedStructure"),
       ("multiple-dashes_and_underscores", "multipleDashesAndUnderscores"),
       ("multiple-dashes_and_underscores", "multipleDashesAndUnderscores"),
-      ("camelCasedKeys", "camelCasedKeys")
+      ("camelCaseKey", "camelCaseKey")
     )
     forAll(examples) { case (input, expected) =>
       ConfigParser.toCamelCase(input) shouldBe expected
     }
   }
 
-  it should "turn all keys in a JSON object into camel case" in {
+  "toSnakeCase" should "turn camelCase keys into SNAKE_CASE" in {
+    val examples = Table(
+      ("input", "expected"),
+      ("nestedStructure", "NESTED_STRUCTURE"),
+      ("nested_structure", "NESTED_STRUCTURE"),
+      ("nested-structure", "NESTED_STRUCTURE")
+    )
+    forAll(examples) { case (input, expected) =>
+      ConfigParser.toSnakeCase(input) shouldBe expected
+    }
+  }
+
+  "withCamelCase" should "turn all keys in a JSON object into camelCase" in {
     val conf = ConfigFactory.load("simple.conf")
     val orig = ConfigParser.toJson(conf.root())
-    val json = (ConfigParser.toCamelCase(orig) \\ "simple").head
+    val json = (ConfigParser.withCamelCase(orig) \\ "simple").head
     json.noSpaces shouldBe """{"nestedStructure":{"barBaz":{"spam":"eggs"},"foo":10}}"""
   }
+
+  "withEnvVarOverrides" should "overwrite keys from the environment" in {
+    val conf = ConfigFactory.load("override.conf")
+    val orig = ConfigParser.toJson(conf.getConfig("override").root())
+    val json = ConfigParser.withCamelCase(orig)
+
+    val env = Map(
+      "TEST_METRICS_ENABLED"     -> "true",
+      "TEST_NETWORK_BOOTSTRAP_0" -> "localhost:50000",
+      "TEST_OPTIONAL"            -> "test",
+      "TEST_NUMERIC"             -> "456",
+      "TEST_TEXTUAL"             -> "Terra Nostra",
+      "TEST_BOOLEAN"             -> "false"
+    )
+
+    val result = ConfigParser.withEnvVarOverrides(json, "TEST", env)
+
+    inside(result) { case Right(json) =>
+      json.noSpaces shouldBe """{"boolean":false,"metrics":{"enabled":true},"network":{"bootstrap":["localhost:50000","localhost:40002"]},"numeric":456,"optional":"test","textual":"Terra Nostra"}"""
+    }
+  }
+
+  it should "validate that data types are not altered" in {
+    val conf = ConfigFactory.load("override.conf")
+    val orig = ConfigParser.toJson(conf.root())
+    val json = ConfigParser.withCamelCase(orig)
+
+    val examples = Table(
+      ("path", "invalid"),
+      ("OVERRIDE_NUMERIC", "NaN"),
+      ("OVERRIDE_TEXTUAL", "123"),
+      ("OVERRIDE_BOOLEAN", "no")
+    )
+    forAll(examples) { case (path, invalid) =>
+      ConfigParser
+        .withEnvVarOverrides(json, "", Map(path -> invalid))
+        .isLeft shouldBe true
+    }
+  }
+
 }
