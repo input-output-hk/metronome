bb148f7a7593baccfb6482d7836c3293e5ea7914
Akosh Farkash <akosh.farkash@iohk.io>
2021-05-05 19:30:21 +0100

    PM-3133: ApplicationService.executeBlocks will need the Commit Q.C.

--- metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/ApplicationService.scala
+++ metronome/hotstuff/service/src/io/iohk/metronome/hotstuff/service/ApplicationService.scala
@@ -1,8 +1,7 @@
 package io.iohk.metronome.hotstuff.service
 
-import cats.data.NonEmptyVector
-import io.iohk.metronome.hotstuff.consensus.basic.Agreement
-import io.iohk.metronome.hotstuff.consensus.basic.QuorumCertificate
+import cats.data.{NonEmptyVector, NonEmptyList}
+import io.iohk.metronome.hotstuff.consensus.basic.{Agreement, QuorumCertificate}
 
 /** Represents the "application" domain to the HotStuff module,
   * performing all delegations that HotStuff can't do on its own.
@@ -16,7 +15,16 @@ trait ApplicationService[F[_], A <: Agreement] {
   def validateBlock(block: A#Block): F[Option[Boolean]]
 
   // TODO (PM-3108, PM-3107, PM-3137, PM-3110): Tell the application to execute a block.
-  def executeBlock(block: A#Block): F[Unit]
+  // I cannot be sure that all blocks that get committed to gether fit into memory,
+  // so we pass them one by one, but all of them are accompanied by the final Commit Q.C.
+  // and the path of block hashes from the block being executed to the one committed.
+  // Perhaps the application service can cache the headers if it needs to produce a
+  // proof of the BFT agreement at the end.
+  def executeBlock(
+      block: A#Block,
+      commitQC: QuorumCertificate[A],
+      commitPath: NonEmptyList[A#Hash]
+  ): F[Unit]
 
   // TODO (PM-3135): Tell the application to sync any state of the block, i.e. the Ledger.
   // The `sources` are peers who most probably have this state.
