4146e75e1ccfb9134ced17219e56704b44dfec23
Akosh Farkash <akosh.farkash@iohk.io>
2021-05-11 22:28:22 +0100

    PM-3241: Block creation, validation and execution.

--- metronome/examples/src/io/iohk/metronome/examples/robot/service/RobotService.scala
+++ metronome/examples/src/io/iohk/metronome/examples/robot/service/RobotService.scala
@@ -1,7 +1,7 @@
 package io.iohk.metronome.examples.robot.service
 
 import cats.implicits._
-import cats.effect.Sync
+import cats.effect.{Sync, Timer}
 import cats.data.{NonEmptyList, NonEmptyVector}
 import io.iohk.metronome.crypto.ECPublicKey
 import io.iohk.metronome.crypto.hash.Hash
@@ -18,67 +18,76 @@ import io.iohk.metronome.hotstuff.service.storage.{
 import io.iohk.metronome.storage.{KVStoreRunner, KVRingBuffer}
 import io.iohk.metronome.storage.KVStoreRead
 import scala.util.Random
+import scala.concurrent.duration._
 
-class RobotService[F[_]: Sync, N](
+class RobotService[F[_]: Sync: Timer, N](
     maxRow: Int,
     maxCol: Int,
     network: Network[F, RobotAgreement, RobotMessage],
     blockStorage: BlockStorage[N, RobotAgreement],
     viewStateStorage: ViewStateStorage[N, RobotAgreement],
-    stateStorage: KVRingBuffer[N, Hash, Robot.State]
+    stateStorage: KVRingBuffer[N, Hash, Robot.State],
+    simulatedDecisionTime: FiniteDuration = 1.second
 )(implicit storeRunner: KVStoreRunner[F, N])
     extends ApplicationService[F, RobotAgreement] {
 
+  /** Make a random valid move on top of the last block. */
   override def createBlock(
       highQC: QuorumCertificate[RobotAgreement]
   ): F[Option[RobotBlock]] = {
-    // Retrieve the blocks that we need to build on.
+    projectState(highQC.blockHash).flatMap {
+      case None =>
+        none.pure[F]
+
+      case Some(preState) =>
+        // Make a valid move; we're not validating our own blocks.
+        // Insert some artifical delay otherwise it will churn out
+        // blocks as fast as it can.
+        Timer[F].sleep(simulatedDecisionTime) >>
+          advanceState(preState).map { case (command, postState) =>
+            RobotBlock(
+              parentHash = highQC.blockHash,
+              postStateHash = postState.hash,
+              command = command
+            ).some
+          }
+    }
+  }
+
+  /** Project state from the last executed block hash to a certain descendant.
+    * Returns None if we don't have the block.
+    */
+  private def projectState(blockHash: Hash): F[Option[Robot.State]] = {
     val query: KVStoreRead[N, (Option[Robot.State], List[RobotBlock])] = for {
       lastExecutedBlockHash <- viewStateStorage.getLastExecutedBlockHash
       blockPath <- blockStorage.getPathFromAncestor(
         lastExecutedBlockHash,
-        highQC.blockHash
+        blockHash
       )
       blocks <- blockPath.traverse(blockStorage.get).map(_.flatten)
-      pendingBlocks = if (blocks.nonEmpty) blocks.tail else Nil
-      maybeLastExecutedState <-
-        pendingBlocks.headOption match {
-          case Some(lastExecutedBlock)
-              if lastExecutedBlock.hash == lastExecutedBlockHash =>
-            stateStorage.get(lastExecutedBlock.postStateHash)
-
-          case _ =>
-            // Shouldn't happen.
-            KVStoreRead.instance[N].pure(None)
-        }
+      pendingBlocks = blocks.drop(1)
+      maybeLastExecutedState <- blocks match {
+        case lastExecutedBlock :: _
+            if lastExecutedBlock.hash == lastExecutedBlockHash =>
+          stateStorage.get(lastExecutedBlock.postStateHash)
+        case _ =>
+          KVStoreRead.instance[N].pure(None)
+      }
     } yield (maybeLastExecutedState, pendingBlocks)
 
-    storeRunner.runReadOnly(query).flatMap {
-      case (None, _) =>
-        Sync[F].raiseError(
-          new IllegalStateException(
-            "Cannot find state corresponding to the last executed block."
-          )
-        )
-
-      case (Some(lastExecutedState), blocks) =>
-        val preState = blocks
-          .foldLeft(lastExecutedState) { case (state, block) =>
-            state.update(block.command)
-          }
-
-        // Make a valid move; we're not validating our own blocks.
-        advance(preState).map { case (command, postState) =>
-          RobotBlock(
-            parentHash = highQC.blockHash,
-            postStateHash = postState.hash,
-            command = command
-          ).some
+    storeRunner.runReadOnly(query).map {
+      case (maybeLastExecutedState, pendingBlocks) =>
+        maybeLastExecutedState.map { lastExecutedState =>
+          pendingBlocks
+            .foldLeft(lastExecutedState) { case (state, block) =>
+              state.update(block.command)
+            }
         }
     }
   }
 
-  private def advance(
+  /** Make a valid move. */
+  private def advanceState(
       preState: Robot.State
   ): F[(Robot.Command, Robot.State)] = {
     import Robot.Command._
@@ -94,24 +103,44 @@ class RobotService[F[_]: Sync, N](
         preState.update(command) match {
           case postState if isValid(postState) =>
             (command, postState).pure[F]
-          case _ => advance(preState)
+          case _ =>
+            advanceState(preState)
         }
       }
   }
 
+  /** Keep the robot within the agreed area. */
   private def isValid(state: Robot.State): Boolean =
     state.position.row >= 0 &&
       state.position.col >= 0 &&
       state.position.row <= maxRow &&
       state.position.col <= maxCol
 
-  override def validateBlock(block: RobotBlock): F[Boolean] = ???
+  /** Validate a block by executing any pending changes and checking the post state. */
+  override def validateBlock(block: RobotBlock): F[Boolean] =
+    projectState(block.parentHash).map {
+      case None =>
+        false
+      case Some(preState) =>
+        val postState = preState.update(block.command)
+        postState.hash == block.postStateHash && isValid(postState)
+    }
 
+  /** Execute the next block in the queue, store the resulting state. */
   override def executeBlock(
       block: RobotBlock,
       commitQC: QuorumCertificate[RobotAgreement],
       commitPath: NonEmptyList[RobotAgreement.Hash]
-  ): F[Unit] = ???
+  ): F[Unit] =
+    projectState(block.parentHash).flatMap {
+      case None =>
+        Sync[F].raiseError(new IllegalStateException("Can't find pre-state."))
+      case Some(preState) =>
+        storeRunner.runReadWrite {
+          val postState = preState.update(block.command)
+          stateStorage.put(postState.hash, postState)
+        }
+    }
 
   def syncState(
       sources: NonEmptyVector[ECPublicKey],
