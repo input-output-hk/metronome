259b913867f848cfd430c640824d681b5271ea60
Akosh Farkash <akosh.farkash@iohk.io>
2021-07-28 15:23:15 +0100

    PM-3562: Validate a checkpointing certificate.

--- metronome/checkpointing/models/src/io/iohk/metronome/checkpointing/models/CheckpointCertificate.scala
+++ metronome/checkpointing/models/src/io/iohk/metronome/checkpointing/models/CheckpointCertificate.scala
@@ -4,6 +4,9 @@ import cats.data.NonEmptyList
 import io.iohk.metronome.hotstuff.consensus.basic.{QuorumCertificate, Phase}
 import io.iohk.metronome.checkpointing.CheckpointingAgreement
 import io.iohk.metronome.checkpointing.models.Transaction.CheckpointCandidate
+import io.iohk.metronome.hotstuff.consensus.basic.Signing
+import io.iohk.metronome.hotstuff.consensus.Federation
+import io.iohk.metronome.core.Validated
 
 /** The Checkpoint Certificate is a proof of the BFT agreement
   * over a given Checkpoint Candidate.
@@ -31,14 +34,65 @@ case class CheckpointCertificate(
 )
 
 object CheckpointCertificate {
+
+  /** Create a `CheckpointCertificate` from a `Block` that last had a `CheckpointCandidate`
+    * and a list of `Block.Header`s leading up to the `QuorumCertifictate` that proves the
+    * BFT agreement over the contents.
+    */
   def construct(
       block: Block,
       headers: NonEmptyList[Block.Header],
       commitQC: QuorumCertificate[CheckpointingAgreement, Phase.Commit]
-  ): Option[CheckpointCertificate] =
+  ): Option[CheckpointCertificate] = {
+    assert(block.hash == headers.head.hash)
     constructProof(block).map { case (proof, cp) =>
       CheckpointCertificate(headers, cp, proof, commitQC)
     }
+  }
+
+  /** Validate a `CheckpointCertificate` by checking that:
+    * - the chain of block headers is valid
+    * - the quorum certificate is valid
+    * - the Merkle proof of the candidate is valid
+    */
+  def validate(
+      certificate: CheckpointCertificate,
+      federation: Federation[CheckpointingAgreement.PKey]
+  )(implicit
+      signing: Signing[CheckpointingAgreement]
+  ): Either[String, Validated[Transaction.CheckpointCandidate]] = {
+    def check(
+        message: String
+    )(cond: Boolean): Either[String, Unit] =
+      Either.cond(cond, (), message)
+
+    for {
+      _ <- check(
+        "The headers do not correspond to a chain of parent-child blocks."
+      ) {
+        certificate.headers.toList.zip(certificate.headers.tail).forall {
+          case (parent, child) =>
+            parent.hash == child.parentHash
+        }
+      }
+
+      _ <- check("The Commit Q.C. is not about the last block in the chain.") {
+        certificate.commitQC.blockHash == certificate.headers.last.hash
+      }
+
+      _ <- check("The Commit Q.C. is invalid.") {
+        signing.validate(federation, certificate.commitQC)
+      }
+
+      _ <- check("The Merkle proof is invalid.") {
+        MerkleTree.verifyProof(
+          certificate.proof,
+          root = certificate.headers.last.contentMerkleRoot,
+          leaf = MerkleTree.Hash(certificate.checkpoint.hash)
+        )
+      }
+    } yield Validated[Transaction.CheckpointCandidate](certificate.checkpoint)
+  }
 
   private def constructProof(
       block: Block
