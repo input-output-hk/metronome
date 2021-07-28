3de1ba0d5300d8443a64971aca68dd7ba69ae583
Akosh Farkash <akosh.farkash@iohk.io>
2021-07-28 22:23:26 +0100

    PM-3562: Test that invalid certificates are rejected.

--- metronome/checkpointing/models/props/src/io/iohk/metronome/checkpointing/models/CheckpointCertificateProps.scala
+++ metronome/checkpointing/models/props/src/io/iohk/metronome/checkpointing/models/CheckpointCertificateProps.scala
@@ -16,13 +16,18 @@ import io.iohk.metronome.hotstuff.consensus.basic.{
 }
 import java.security.SecureRandom
 import org.scalacheck._
-import org.scalacheck.Prop.{forAll, propBoolean}
+import org.scalacheck.Prop.{forAll, forAllNoShrink, propBoolean}
 import scodec.bits.ByteVector
+import scala.util.Random
 
 object CheckpointCertificateProps extends Properties("CheckpointCertificate") {
   import ArbitraryInstances._
   import Arbitrary.arbitrary
   import RLPCodecs._
+
+  // Testing with real signatures is rather slow.
+  override def overrideParameters(p: Test.Parameters): Test.Parameters =
+    p.withMinSuccessfulTests(20)
 
   implicit val genFederation: Gen[
     (
@@ -98,6 +103,65 @@ object CheckpointCertificateProps extends Properties("CheckpointCertificate") {
 
   } yield (federation, certificate)
 
+  def invalidateCertificate(
+      c: CheckpointCertificate
+  ): Gen[(String, CheckpointCertificate)] =
+    Gen.oneOf(
+      arbitrary[Block.Hash].map { h =>
+        "Invalid block hash" -> c.copy(commitQC =
+          c.commitQC.copy[CheckpointingAgreement, Phase.Commit](blockHash = h)
+        )
+      },
+      arbitrary[ViewNumber].map { vn =>
+        "Invalid view number" -> c.copy(commitQC =
+          c.commitQC.copy[CheckpointingAgreement, Phase.Commit](viewNumber =
+            ViewNumber(c.commitQC.viewNumber + vn)
+          )
+        )
+      },
+      Gen.oneOf(c.commitQC.signature.sig).map { s =>
+        "Removed signature" -> c.copy(commitQC =
+          c.commitQC.copy[CheckpointingAgreement, Phase.Commit](signature =
+            c.commitQC.signature.copy(
+              sig = c.commitQC.signature.sig.filterNot(_ == s)
+            )
+          )
+        )
+      },
+      arbitrary[CheckpointingAgreement.PSig].map { s =>
+        "Replaced signature" -> c.copy(commitQC =
+          c.commitQC.copy[CheckpointingAgreement, Phase.Commit](signature =
+            c.commitQC.signature.copy(
+              sig = s +: c.commitQC.signature.sig.tail
+            )
+          )
+        )
+      },
+      arbitrary[Transaction.CheckpointCandidate].map { cc =>
+        "Invalid candidate" -> c.copy(checkpoint = cc)
+      },
+      arbitrary[CheckpointCertificate].map { cc =>
+        "Invalid proof" -> c.copy(proof = cc.proof)
+      },
+      for {
+        hs0 <- Gen.const(c.headers.toList)
+        if hs0.size > 1
+        seed <- arbitrary[Int]
+        hs1 = new Random(seed).shuffle(hs0)
+        if hs1 != hs0
+      } yield "Shuffled headers" -> c.copy(headers =
+        NonEmptyList.fromListUnsafe(hs1)
+      ),
+      Gen.nonEmptyListOf(arbitrary[Block.Header]).map { bs =>
+        "Prefixed headers" ->
+          c.copy(headers = NonEmptyList.fromListUnsafe(bs ++ c.headers.toList))
+      },
+      Gen.nonEmptyListOf(arbitrary[Block.Header]).map { bs =>
+        "Postfixed headers" ->
+          c.copy(headers = NonEmptyList.fromListUnsafe(c.headers.toList ++ bs))
+      }
+    )
+
   property("validate - reject random") = forAll {
     (
         federation: Federation[CheckpointingAgreement.PKey],
@@ -116,5 +180,21 @@ object CheckpointCertificateProps extends Properties("CheckpointCertificate") {
         )
   }
 
-  property("validate - reject invalid") = true
+  property("validate - reject invalid") = forAllNoShrink(
+    for {
+      (federation, valid) <- validCertificate
+      (hint, invalid)     <- invalidateCertificate(valid)
+    } yield (federation, hint, invalid)
+  ) { case (federation, hint, certificate) =>
+    hint |: CheckpointCertificate.validate(certificate, federation).isLeft
+  }
+
+  property("validate - reject foreign") = forAll(
+    for {
+      (_, valid) <- validCertificate
+      federation <- arbitrary[Federation[CheckpointingAgreement.PKey]]
+    } yield (federation, valid)
+  ) { case (federation, certificate) =>
+    CheckpointCertificate.validate(certificate, federation).isLeft
+  }
 }
