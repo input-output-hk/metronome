00dea4d044f8e86eb7e34f1d36989933cb66568f
Akosh Farkash <akosh.farkash@iohk.io>
2021-08-31 16:13:19 +0100

    PM-3144: Configuration and parser.

--- metronome/checkpointing/app/src/io/iohk/metronome/checkpointing/app/config/CheckpointingConfig.scala
+++ metronome/checkpointing/app/src/io/iohk/metronome/checkpointing/app/config/CheckpointingConfig.scala
@@ -1,3 +1,35 @@
 package io.iohk.metronome.checkpointing.app.config
 
-case class CheckpointingConfig()
+import io.iohk.metronome.crypto.{ECPublicKey, ECPrivateKey}
+import java.nio.file.Path
+import scala.concurrent.duration.FiniteDuration
+
+case class CheckpointingConfig(
+    federation: CheckpointingConfig.Federation,
+    network: CheckpointingConfig.Network,
+    db: CheckpointingConfig.Database
+)
+
+object CheckpointingConfig {
+  case class Federation(
+      others: List[Node],
+      self: Node,
+      privateKey: ECPrivateKey
+  )
+  case class Node(
+      host: String,
+      port: Int,
+      publicKey: ECPublicKey
+  )
+  case class Network(
+      bindHost: String,
+      bindPort: Int,
+      timeout: FiniteDuration
+  )
+  case class Database(
+      path: Path,
+      stateHistorySize: Int,
+      blockHistorySize: Int,
+      pruneInterval: FiniteDuration
+  )
+}
--- metronome/checkpointing/app/src/io/iohk/metronome/checkpointing/app/config/CheckpointingConfigParser.scala
+++ metronome/checkpointing/app/src/io/iohk/metronome/checkpointing/app/config/CheckpointingConfigParser.scala
@@ -2,7 +2,11 @@ package io.iohk.metronome.checkpointing.app.config
 
 import com.typesafe.config.ConfigFactory
 import io.iohk.metronome.config.{ConfigParser, ConfigDecoders}
+import io.iohk.metronome.crypto.{ECPublicKey, ECPrivateKey}
 import io.circe._, io.circe.generic.semiauto._
+import java.nio.file.Path
+import scodec.bits.ByteVector
+import scala.util.Try
 
 object CheckpointingConfigParser {
   def parse: ConfigParser.Result[CheckpointingConfig] = {
@@ -11,6 +15,39 @@ object CheckpointingConfigParser {
       prefix = "METRONOME_CHECKPOINTING"
     )
   }
+
+  import ConfigDecoders._
+
+  def hexDecoder[T](f: ByteVector => T): Decoder[T] =
+    Decoder[String].emap { str =>
+      ByteVector.fromHex(str) match {
+        case None =>
+          Left("$str is not a valid hexadecimal value")
+        case Some(bytes) =>
+          Try(f(bytes)).toEither.left.map(_.getMessage)
+      }
+    }
+
+  implicit val ecPublicKeyDecoder: Decoder[ECPublicKey] =
+    hexDecoder(ECPublicKey(_))
+
+  implicit val ecPrivateKeyDecoder: Decoder[ECPrivateKey] =
+    hexDecoder(ECPrivateKey(_))
+
+  implicit val nodeDecoder: Decoder[CheckpointingConfig.Node] =
+    deriveDecoder
+
+  implicit val federationDecoder: Decoder[CheckpointingConfig.Federation] =
+    deriveDecoder
+
+  implicit val networkDecoder: Decoder[CheckpointingConfig.Network] =
+    deriveDecoder
+
+  implicit val pathDecoder: Decoder[Path] =
+    Decoder[String].map(Path.of(_))
+
+  implicit val dbDecoder: Decoder[CheckpointingConfig.Database] =
+    deriveDecoder
 
   implicit val configDecoder: Decoder[CheckpointingConfig] = deriveDecoder
 }
